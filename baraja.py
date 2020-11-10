e82d70d5b20d54c3c901268c85a68b1612d221ec
Marta Maquedano <marta.maquedano@gmail.com>
2020-11-10 22:14:03 +0100

    primer commit baraja

---
+++ baraja.py
@@ -0,0 +1,9 @@
+palos = ['o', 'c', 'e', 'b']
+numeros = ['A', '2', '3','4', '5', '6', '7', 'S', 'C', 'R']
+baraja = []
+
+
+for palo in palos:
+    for numero in numeros:
+        print(numero+palo)
+        
