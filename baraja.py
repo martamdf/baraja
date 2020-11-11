208b781783f0922f718012eb074680e1874425fa
Marta Maquedano <marta.maquedano@gmail.com>
2020-11-11 23:02:44 +0100

    baraja con función barajar

--- baraja.py
+++ baraja.py
@@ -1,3 +1,5 @@
+import random
+
 palos = ['o', 'c', 'e', 'b']
 numeros = ['A', '2', '3','4', '5', '6', '7', 'S', 'C', 'R']
 
@@ -10,3 +12,13 @@ def creaBaraja():
     return baraja
         
 creaBaraja()
+
+
+def barajar(lista_de_naipes):
+    for i in range(len(lista_de_naipes)):
+        nueva_pos = random.randrange(len(lista_de_naipes))
+
+        aux = lista_de_naipes[nueva_pos]
+        lista_de_naipes[nueva_pos] = lista_de_naipes[i]
+        lista_de_naipes[i]=aux
+    return lista_de_naipes
